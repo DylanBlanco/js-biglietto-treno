9f4d69b2ed43f6852000fb0c2d9a75272bae7b3b
DylanBlanco <dylanblanco1198@gmail.com>
2024-07-11 13:45:43 +0200

    js - step 1 e 2 (chiedi km e nome)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//chiedi n. Km da percorrere e salva valore
+const distance =  prompt(`Inserisci km da percorrere?`);
+console.log (`km da percorrere:`, distance);
+
+//chiedi nome e salva valore
+const UserName =  prompt(`Inserisci Nome?`);
+console.log (`Il nome dell'utente è:`, UserName);
