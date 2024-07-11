d8e8330961ad952c8cbcf4528dfc9072feb59498
DylanBlanco <dylanblanco1198@gmail.com>
2024-07-12 01:48:23 +0200

    stampa prezzo biglietto

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-//chiedi n. Km da percorrere e salva valore
-const distance =  prompt(`Inserisci km da percorrere?`);
-console.log (`km da percorrere:`, distance);
+//chiedi km
+const km = prompt (`Quanti km vuoi percorrere?:`);
+console.log (`km percorsi:`, km);
 
-//chiedi nome e salva valore
-const UserName =  prompt(`Inserisci Nome?`);
-console.log (`Il nome dell'utente è:`, UserName);
+//chiedi età
+const age = prompt (`Quanti anni hai?:`);
+console.log (`età utente:`, age);
+
+//prezzo biglietto
+const prezzokm = 0.21
+let prezzofinale = km * prezzokm;
+console.log (`prezzo biglietto:`, prezzokm);
+
+//calcola prezzo under e over
+if (age <= 18) {
+    prezzofinale=prezzofinale * 0.8;
+    console.log ('Prezzo Scontato Under 18:', prezzofinale);
+}
+else if (age >= 65) {
+    prezzofinale = prezzofinale * 0.6;
+    console.log('Prezzo Scontato Over 65:', prezzofinale);
+}
+
+//stampa prezzo biglietto
+document.getElementById('prezzofinale').innerHTML= prezzofinale.toFixed(2);
